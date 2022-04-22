1486e784bdc5188dfc88643c37164af5d660713c
panthuncia <matthew.h.gomes@gmail.com>
2022-04-22 17:47:31 -0400

    complete communication

--- src/ComArduino2.py
+++ src/ComArduino2.py
@@ -50,99 +50,99 @@
 #
 
 
-#=====================================
+# =====================================
 
 #  Function Definitions
 
-#=====================================
+# =====================================
 
 def sendToArduino(sendStr):
-  a=bytes("<", 'utf-8')
-  print("A is "+str(a))
-  print("sendStr is "+str(sendStr))
-  ser.write(sendStr)
+    a = bytes("<", 'utf-8')
+    print("A is " + str(a))
+    print("sendStr is " + str(sendStr))
+    ser.write(sendStr)
 
 
-#======================================
+# ======================================
 
 def recvFromArduino():
-  global startMarker, endMarker
-  
-  ck = b''
-  x = "z" # any value that is not an end- or startMarker
-  byteCount = -1 # to allow for the fact that the last increment will be one too many
-  
-  # wait for the start character
-  while  ord(x) != startMarker: 
-    x = ser.read()
-  
-  # save data until the end marker is found
-  while ord(x) != endMarker:
-    if ord(x) != startMarker:
-      ck = ck + x 
-      byteCount += 1
-    x = ser.read()
-  
-  return(ck)
+    global startMarker, endMarker
+
+    ck = b''
+    x = "z"  # any value that is not an end- or startMarker
+    byteCount = -1  # to allow for the fact that the last increment will be one too many
+
+    # wait for the start character
+    while ord(x) != startMarker:
+        x = ser.read()
+
+    # save data until the end marker is found
+    while ord(x) != endMarker:
+        if ord(x) != startMarker:
+            ck = ck + x
+            byteCount += 1
+        x = ser.read()
+
+    return (ck)
 
 
-#============================
+# ============================
 
 def waitForArduino():
+    # wait until the Arduino sends 'Arduino Ready' - allows time for Arduino reset
+    # it also ensures that any bytes left over from a previous message are discarded
 
-   # wait until the Arduino sends 'Arduino Ready' - allows time for Arduino reset
-   # it also ensures that any bytes left over from a previous message are discarded
-   
     global startMarker, endMarker
-    
+
     msg = b''
-    while msg.find(bytes("Arduino is ready", encoding = 'utf8')) == -1:
+    while msg.find(bytes("Arduino is ready", encoding='utf8')) == -1:
 
-      while ser.inWaiting() == 0:
-        pass
-        
-      msg = recvFromArduino()
+        while ser.inWaiting() == 0:
+            pass
 
-      print(msg)
-      print("")
-      
-#======================================
+        msg = recvFromArduino()
+
+        print(msg)
+        print("")
+
+
+# ======================================
 
 def runTest(td):
-  numLoops = len(td)
-  waitingForReply = False
+    numLoops = len(td)
+    waitingForReply = False
 
-  n = 0
-  while n < numLoops:
+    n = 0
+    while n < numLoops:
 
-    teststr = td[n].encode(ascii)
-    print("sending "+str(teststr));
+        teststr = td[n].encode("ascii")
+        print("sending " + str(teststr));
 
-    if waitingForReply == False:
-      sendToArduino(teststr)
-      print("Sent from PC -- LOOP NUM " + str(n) + " TEST STR " + str(teststr))
-      waitingForReply = True
+        if not waitingForReply:
+            sendToArduino(teststr)
+            print("Sent from PC -- LOOP NUM " + str(n) + " TEST STR " + str(teststr))
+            waitingForReply = True
 
-    if waitingForReply == True:
+        if waitingForReply:
 
-      while ser.inWaiting() == 0:
-        pass
-        
-      dataRecvd = recvFromArduino()
-      print("Reply Received  " + str(dataRecvd))
-      n += 1
-      waitingForReply = False
+            while ser.inWaiting() == 0:
+                pass
 
-      print("===========")
+            dataRecvd = recvFromArduino()
+            print("Reply Received  " + str(dataRecvd))
+            n += 1
+            waitingForReply = False
 
-    time.sleep(5)
+            print("===========")
+
+        #time.sleep()
 
 
-#======================================
+# ======================================
 
 # THE DEMO PROGRAM STARTS HERE
 
-#======================================
+# ======================================
 
 import serial
 import time
@@ -156,19 +156,17 @@ baudRate = 9600
 ser = serial.Serial(serPort, baudRate)
 print("Serial port " + serPort + " opened  Baudrate " + str(baudRate))
 
-
 startMarker = 60
 endMarker = 62
-
 
 waitForArduino()
 
 print("Finished waiting")
-testData = "<LED1,100,0.2>"
-print(testData)
-
-runTest(testData)
-
+testData = "<270,270,270,270,270,270>"
+testData1 = "<180,180,180,180,180,180>"
+testData = "<270,180,180,180,180,180>"
+testData = "<0,0,0,0,0,0>"
+testData1 = "<70,70,70,70,70,70>"
+runTest(testData1)
 
 ser.close
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,46 @@
 #include <Arduino.h>
 #include <Adafruit_PWMServoDriver.h>
 
-#define SERVOMIN  100.0 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  470.0 // This is the 'maximum' pulse length count (out of 4096)
-#define SERVO_FREQ 50.0 // Analog servos run at ~50 Hz updates
+#define SERVO0MIN  115 // 0 degrees
+#define SERVO0MAX  480 // 270 degrees
+#define SERVO1MIN  111 // 0 degrees
+#define SERVO1MAX  473 // 270 degrees
+#define SERVO2MIN  116 // 0 degrees
+#define SERVO2MAX  477 // 270 degrees
+#define SERVO3MIN  121 // 0 degrees
+#define SERVO3MAX  486 // 270 degrees
+#define SERVO4MIN  124 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVO4MAX  494 // This is the 'maximum' pulse length count (out of 4096)
+#define SERVO5MIN  118 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVO5MAX  479 // This is the 'maximum' pulse length count (out of 4096)
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+
+double SERVO0DIFF;
+double SERVO1DIFF;
+double SERVO2DIFF;
+double SERVO3DIFF;
+double SERVO4DIFF;
+double SERVO5DIFF;
+
 void getDataFromPC();
-void updateFlashInterval();
-void updateServoPos();
 void replyToPC();
-void flashLEDs();
-void moveServo();
+void moveServos();
 void parseData();
-void updateLED1(); 
-void updateLED2(); 
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-// our servo # counter
-uint8_t servonum = 0;
-byte servoPos = 0;
-byte newServoPos = SERVOMIN;
-
-const byte numLEDs = 1;
-byte ledPin[numLEDs] = {13};
-unsigned long LEDinterval[numLEDs] = {200};
-unsigned long prevLEDmillis[numLEDs] = {0};
+uint16_t newServo0Pos = SERVO0MAX;
+uint16_t servo0Pos = SERVO0MIN-1;
+uint16_t newServo1Pos = SERVO1MAX;
+uint16_t servo1Pos = SERVO1MIN-1;
+uint16_t newServo2Pos = SERVO2MAX;
+uint16_t servo2Pos = SERVO2MIN-1;
+uint16_t newServo3Pos = SERVO3MAX;
+uint16_t servo3Pos = SERVO3MIN-1;
+uint16_t newServo4Pos = SERVO4MAX;
+uint16_t servo4Pos = SERVO4MIN-1;
+uint16_t newServo5Pos = SERVO5MAX;
+uint16_t servo5Pos = SERVO5MIN-1;
 
 const byte buffSize = 40;
 char inputBuffer[buffSize];
@@ -35,9 +51,6 @@ boolean readInProgress = false;
 boolean newDataFromPC = false;
 
 char messageFromPC[buffSize] = {0};
-int newFlashInterval = 0;
-float servoFraction = 0.0; // fraction of servo range to move
-
 
 unsigned long curMillis;
 
@@ -45,6 +58,12 @@ unsigned long prevReplyToPCmillis = 0;
 unsigned long replyToPCinterval = 1000;
 
 void setup() {
+  SERVO0DIFF=(SERVO0MAX - SERVO0MIN);
+  SERVO1DIFF=(SERVO1MAX - SERVO1MIN);
+  SERVO2DIFF=(SERVO2MAX - SERVO2MIN);
+  SERVO3DIFF=(SERVO3MAX - SERVO3MIN);
+  SERVO4DIFF=(SERVO4MAX - SERVO5MIN);
+  SERVO5DIFF=(SERVO5MAX - SERVO5MIN);
   Serial.begin(9600);
   Serial.println("8 channel Servo test!");
 
@@ -65,40 +84,22 @@ void setup() {
    * affects the calculations for the PWM update frequency. 
    * Failure to correctly set the int.osc value will cause unexpected PWM results
    */
-  pwm.setOscillatorFrequency(26000000);
+  pwm.setOscillatorFrequency(24100000);
   pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
 
   delay(10);
-  
-    // flash LEDs so we know we are alive
-  for (byte n = 0; n < numLEDs; n++) {
-     pinMode(ledPin[n], OUTPUT);
-     digitalWrite(ledPin[n], HIGH);
-  }
-  delay(500); // delay() is OK in setup as it only happens once
-  
-  for (byte n = 0; n < numLEDs; n++) {
-     digitalWrite(ledPin[n], LOW);
-  }
-  
     // initialize the servo
-  moveServo();
+  moveServos();
   
     // tell the PC we are ready
   Serial.println("<Arduino is ready>");
 }
 
 void loop() {
-  // Drive each servo one at a time using setPWM()
-  //pwm.setPWM(0, 0, 470);
-
   curMillis = millis();
   getDataFromPC();
-  updateFlashInterval();
-  updateServoPos();
   replyToPC();
-  flashLEDs();
-  moveServo();
+  moveServos();
 }
 
 //=============
@@ -142,13 +143,53 @@ void parseData() {
   char * strtokIndx; // this is used by strtok() as an index
   
   strtokIndx = strtok(inputBuffer,",");      // get the first part - the string
-  strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
-  
-  strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
-  newFlashInterval = atoi(strtokIndx);     // convert this part to an integer
-  
-  strtokIndx = strtok(NULL, ","); 
-  servoFraction = atof(strtokIndx);     // convert this part to a float
+  newServo0Pos = atoi(strtokIndx);     // convert this part to an int
+  newServo0Pos = (((newServo0Pos) * SERVO0DIFF) / (270)) + SERVO0MIN;
+  if(newServo0Pos<SERVO0MIN){
+    newServo0Pos=SERVO0MIN;
+  } else if(newServo0Pos>SERVO0MAX){
+    newServo0Pos=SERVO0MAX;
+  }
+  strtokIndx = strtok(NULL,",");      // get the first part - the string
+  newServo1Pos = atoi(strtokIndx);     // convert this part to an int
+  newServo1Pos = (((newServo1Pos) * SERVO1DIFF) / (270)) + SERVO1MIN;
+  if(newServo1Pos<SERVO1MIN){
+    newServo1Pos=SERVO1MIN;
+  } else if(newServo1Pos>SERVO1MAX){
+    newServo1Pos=SERVO1MAX;
+  }
+  strtokIndx = strtok(NULL,",");      // get the first part - the string
+  newServo2Pos = atoi(strtokIndx);     // convert this part to an int
+  newServo2Pos = (((newServo2Pos) * SERVO2DIFF) / (270)) + SERVO2MIN;
+  if(newServo2Pos<SERVO2MIN){
+    newServo2Pos=SERVO2MIN;
+  } else if(newServo2Pos>SERVO2MAX){
+    newServo2Pos=SERVO2MAX;
+  }
+  strtokIndx = strtok(NULL,",");      // get the first part - the string
+  newServo3Pos = atoi(strtokIndx);     // convert this part to an int
+  newServo3Pos = (((newServo3Pos) * SERVO3DIFF) / (270)) + SERVO3MIN;
+  if(newServo3Pos<SERVO3MIN){
+    newServo3Pos=SERVO3MIN;
+  } else if(newServo3Pos>SERVO3MAX){
+    newServo3Pos=SERVO3MAX;
+  }
+  strtokIndx = strtok(NULL,",");      // get the first part - the string
+  newServo4Pos = atoi(strtokIndx);     // convert this part to an int
+  newServo4Pos = (((newServo4Pos) * SERVO4DIFF) / (270)) + SERVO4MIN;
+  if(newServo4Pos<SERVO4MIN){
+    newServo4Pos=SERVO4MIN;
+  } else if(newServo4Pos>SERVO4MAX){
+    newServo4Pos=SERVO4MAX;
+  }
+  strtokIndx = strtok(NULL,",");      // get the first part - the string
+  newServo5Pos = atoi(strtokIndx);     // convert this part to an int
+  newServo5Pos = (((newServo5Pos) * SERVO5DIFF) / (270)) + SERVO5MIN;
+  if(newServo5Pos<SERVO5MIN){
+    newServo5Pos=SERVO5MIN;
+  } else if(newServo5Pos>SERVO5MAX){
+    newServo5Pos=SERVO5MAX;
+  }
 
 }
 
@@ -158,79 +199,37 @@ void replyToPC() {
 
   if (newDataFromPC) {
     newDataFromPC = false;
-    Serial.print("<Msg ");
-    Serial.print(messageFromPC);
-    Serial.print(" NewFlash ");
-    Serial.print(newFlashInterval);
-    Serial.print(" SrvFrac ");
-    Serial.print(servoFraction);
-    Serial.print(" SrvPos ");
-    Serial.print(newServoPos);
-    Serial.print(" Time ");
-    Serial.print(curMillis >> 9); // divide by 512 is approx = half-seconds
+    Serial.print("<Time ");
+    Serial.print(millis()-curMillis);
     Serial.println(">");
   }
 }
 
-//============
-
-void updateFlashInterval() {
-
-   // this illustrates using different inputs to call different functions
-  if (strcmp(messageFromPC, "LED1") == 0) {
-     updateLED1();
-  }
-  
-  if (strcmp(messageFromPC, "LED2") == 0) {
-     updateLED2();
-  }
-}
-
 //=============
 
-void updateLED1() {
-
-  if (newFlashInterval > 100) {
-    LEDinterval[0] = newFlashInterval;
+void moveServos() {
+  if (servo0Pos != newServo0Pos) {
+    servo0Pos = newServo0Pos;
+    pwm.setPWM(0, 0, newServo0Pos);
   }
-}
-
-//=============
-
-void updateLED2() {
-
-  if (newFlashInterval > 100) {
-    LEDinterval[1] = newFlashInterval;
+  if (servo1Pos != newServo1Pos) {
+    servo1Pos = newServo1Pos;
+    pwm.setPWM(2, 0, newServo1Pos);
   }
-}
-
-//=============
-
-void flashLEDs() {
-
-  for (byte n = 0; n < numLEDs; n++) {
-    if (curMillis - prevLEDmillis[n] >= LEDinterval[n]) {
-       prevLEDmillis[n] += LEDinterval[n];
-       digitalWrite( ledPin[n], ! digitalRead( ledPin[n]) );
-    }
+  if (servo2Pos != newServo2Pos) {
+    servo2Pos = newServo2Pos;
+    pwm.setPWM(4, 0, newServo2Pos);
   }
-}
-
-//=============
-
-void updateServoPos() {
-
-  double servoRange = SERVOMAX - SERVOMIN;
-  if (servoFraction >= 0 && servoFraction <= 1) {
-    newServoPos = SERVOMIN + (servoRange * servoFraction);
+  if (servo3Pos != newServo3Pos) {
+    servo3Pos = newServo3Pos;
+    pwm.setPWM(6, 0, newServo3Pos);
   }
-}
-
-//=============
-
-void moveServo() {
-  if (servoPos != newServoPos) {
-    servoPos = newServoPos;
-    pwm.setPWM(0, 0, newServoPos);
+  if (servo4Pos != newServo4Pos) {
+    servo4Pos = newServo4Pos;
+    pwm.setPWM(8, 0, newServo4Pos);
+  }
+  if (servo5Pos != newServo5Pos) {
+    servo5Pos = newServo5Pos;
+    pwm.setPWM(10, 0, newServo5Pos);
   }
 }
